3574282335b467ade2a220bf145c99909d32e990
amoko2021 <83492837+amoko2021@users.noreply.github.com>
2022-09-11 16:39:36 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,64 @@
+<h3 align="center">Spin the wheel bot</h3>
+<p align="center">
+  A telegram bot for selecting a random choice
+<p>
+
+<!-- TABLE OF CONTENTS -->
+<details open="open">
+  <summary><h2 style="display: inline-block">Table of Contents</h2></summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This is a telegram bot that allows the user to populate the choices, then selects a random choice from it.
+
+### Built With
+
+* python==3.7.10
+* python-telegram-bot==12.4.2
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+Install python-telegram-bot with
+```sh
+python3 -m pip install -r requirements.txt
+```
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/qijie44/spinthewheelbot.git
+   ```
+2. Generate a new bot using [botfather](https://t.me/botfather) and copy the token generated
+3. Paste the token into bot.ini, replacing YOUR_BOT_TOKEN_HERE and save.
+4. Run the bot.py using python
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+This project is provided as it is. Please do not contact me.
---
+++ bot.py
@@ -0,0 +1,72 @@
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler, Filters
+import configparser
+import logging
+import random
+import datetime
+
+config = configparser.ConfigParser()
+config.read("bot.ini")
+data = {}
+
+updater = Updater(token=config["KEYS"]["BOT_TOKEN"], use_context="true")
+dispatcher = updater.dispatcher
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id= update.effective_chat.id, text="I'm a bot, please talk to me!")
+    print(update.effective_user.id)
+
+def spin_the_wheel(update, context):
+    #instantiate an empty array with the chat id
+    data[update.effective_chat.id]= [[], 0]
+    context.bot.send_message(chat_id= update.effective_chat.id, text="Wheel started! Please type in the choices.")
+
+def add(update, context):
+    # if the array exists, add to it and inform the user
+    if (update.effective_chat.id in data) and (context.args != []):
+        sentence = ""
+        for i in range(len(context.args)):
+            sentence += context.args[i] +" "
+        data[update.effective_chat.id][0].append(sentence)
+        message = sentence + " added!"
+    elif (context.args == []):
+        message = "Please add a choice after the command!"
+    context.bot.send_message(chat_id= update.effective_chat.id, text=message)
+
+def remove(update, context):
+    # if the array exists, remove from it and inform the user
+    if (update.effective_chat.id in data) and (context.args != []):
+        try:
+            sentence = ""
+            for i in range(len(context.args)):
+                sentence += context.args[i] +" "
+            data[update.effective_chat.id][0].remove(sentence)
+            message = sentence + " removed!"
+        except:
+            message = sentence + " does not exist!"
+    elif (context.args == []):
+        message = "Please add a choice after the command!"
+    context.bot.send_message(chat_id= update.effective_chat.id, text=message)
+
+def spin(update, context):
+    if (update.effective_chat.id in data) and (data[update.effective_chat.id][1] != []):
+        # returns a random choice from the list and delete the list
+        choice = random.choice(data[update.effective_chat.id][0])
+        data[update.effective_chat.id][1] = datetime.datetime.now()
+        message = choice + " was selected!"
+        context.bot.send_message(chat_id= update.effective_chat.id, text=message)
+        for user in data:
+            if ((datetime.datetime.now() - data[user][1]).seconds) >= 120:
+                del data[user]
+    else:
+        context.bot.send_message(chat_id= update.effective_chat.id, text="Please run the /spin_the_wheel command to start a wheel!")
+
+dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("spin_the_wheel", spin_the_wheel))
+dispatcher.add_handler(CommandHandler("spin", spin))
+dispatcher.add_handler(CommandHandler("add", add))
+dispatcher.add_handler(CommandHandler("remove", remove))
+updater.start_polling()
